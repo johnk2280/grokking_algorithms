ce9af65bc4aea36378a3d4a0a590b5525d3e94a3
johnk2280 <johnk2280@gmail.com>
2022-08-25 12:01:37 +0500

    [add] central_element_search.py

--- linkedList/tasks/central_element_search.py
+++ linkedList/tasks/central_element_search.py
@@ -1,6 +1,29 @@
+"""Найдите центральный элемент списка за один проход?"""
+
 from linkedList.linked_list import LinkedList
 from linkedList.linked_list import Node
 
+a = Node(1)
+b = Node(3)
+c = Node(19)
+d = Node(15)
+e = Node(8)
+f = Node(11)
+
+ll = LinkedList()
+for node in [a, b, c, d, e, f]:
+    ll.add_in_tail(node)
+
+
+def get_central_element(lst: LinkedList) -> Node:
+    fast = lst.head
+    slow = lst.head
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+
+    return slow
+
 
 if __name__ == '__main__':
-    print('ok')
+    print(get_central_element(ll) == d)
